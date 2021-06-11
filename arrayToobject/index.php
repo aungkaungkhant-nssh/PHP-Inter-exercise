eb29c2b64e84ecdcbe76ea4856940c9a795d8db8
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-06-11 20:16:11 +0630

    arrayToObject

---
+++ arrayToobject/index.php
@@ -0,0 +1,9 @@
+<?php
+$arry=["one"=>"1","two"=>"2","three"=>"3"];
+$obj=(object)$arry;
+echo $obj->one;
+
+// booelan
+$bol=False;
+$str=(int)$bol;
+echo $str;
--- pollymorphism1/index.php
+++ pollymorphism1/index.php
@@ -1,14 +1,21 @@
 <?php
 include("./vendor/autoload.php");
 use App\Alphard;
+use App\Gen\Rule;
 use App\Marcity;
 class Detail{
     public function __construct()
     {
         $alphard=new Alphard();
-        $alphard->price();
+        // $alphard->price();
         $marcity=new Marcity();
-        $marcity->price();
+        // $marcity->price();
+        $this->getYear($alphard);
+        $this->getYear($marcity);
+    }
+    public function getYear(Rule $rule){
+        $yr=$rule->year();
+        echo $yr;
     }
 }
 $detail=new Detail();
